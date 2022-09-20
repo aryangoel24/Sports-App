8a2cbec42d7b85c3399b54f1e47c8ad0c1d055d9
aryangoel24 <aryangoel24@gmail.com>
2022-09-19 21:22:17 -0400

    new api

--- src/components/Standings.js
+++ src/components/Standings.js
@@ -3,8 +3,8 @@ import React, { useState, useEffect } from "react";
 import {BallTriangle} from "react-loader-spinner";
 
 const Standings = () => {
-  const [selectedLeague, setSelectedLeague] = useState("eng.1");
-  const [selectedYear, setSelectedYear] = useState("2022");
+  const [selectedLeague, setSelectedLeague] = useState("4328");
+  const [selectedYear, setSelectedYear] = useState("2022-2023");
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
 
@@ -13,11 +13,12 @@ const Standings = () => {
     setData([]);
     axios
       .get(
-        `https://api-football-standings.azharimm.site/leagues/${selectedLeague}/standings?season=${selectedYear}`
+        `https://www.thesportsdb.com/api/v1/json/2/lookuptable.php?l=${selectedLeague}&s=${selectedYear}`
+        // `https://api-football-standings.azharimm.site/leagues/${selectedLeague}/standings?season=${selectedYear}`
       )
       .then((res) => {
-        console.log(res.data.data.standings);
-        setData(res.data.data.standings);
+        console.log(res.data.table);
+        setData(res.data.table);
       })
       .catch((err) => console.log(err))
       .finally(() => setLoading(false));
@@ -32,26 +33,18 @@ const Standings = () => {
           defaultValue={selectedLeague}
           onChange={(e) => setSelectedLeague(e.target.value)}
         >
-          <option value="arg.1">Argentine Liga Profesional de Fútbol</option>
-          <option value="aus.1">Australian A-League</option>
-          <option value="bra.1">Brazilian Serie A</option>
-          <option value="chn.1">Chinese Super League</option>
-          <option value="ned.1">Dutch Eredivisie</option>
-          <option value="eng.1">English Premier League</option>
-          <option value="fra.1">French Ligue 1</option>
-          <option value="ger.1">German Bundesliga</option>
-          <option value="idn.1">Indonesian Liga 1</option>
-          <option value="ita.1">Italian Serie A</option>
-          <option value="jpn.1">Japanese J League</option>
-          <option value="mys1">Malaysian Super League</option>
-          <option value="mex.1">Mexican Liga BBVA MX</option>
-          <option value="por.1">Portuguese Liga</option>
-          <option value="rus.1">Russian Premier League</option>
-          <option value="sgp.1">Singaporean Premier League</option>
-          <option value="esp.1">Spanish Primera División</option>
-          <option value="tha.1">Thai Premier League</option>
-          <option value="tur.1">Turkish Super Lig</option>
-          <option value="uga.1">Ugandan Super League</option>
+          <option value="4328">English Premier League</option>
+          <option value="4335">Spanish Primera División</option>
+          <option value="4332">Italian Serie A</option>
+          <option value="4331">German Bundesliga</option>
+          <option value="4334">French Ligue 1</option>
+          <option value="4356">Australian A-League</option>
+          <option value="4337">Dutch Eredivisie</option>
+          <option value="4790">Indonesian Liga 1</option>
+          <option value="4350">Mexican Liga BBVA MX</option>
+          <option value="4344">Portuguese Liga</option>
+          <option value="4355">Russian Premier League</option>
+          
         </select>
         <select
           name="select-year"
@@ -59,18 +52,19 @@ const Standings = () => {
           onChange={(e) => setSelectedYear(e.target.value)}
           defaultValue={selectedYear}
         >
-          <option value="2011">2011</option>
-          <option value="2012">2012</option>
-          <option value="2013">2013</option>
-          <option value="2014">2014</option>
-          <option value="2015">2015</option>
-          <option value="2016">2016</option>
-          <option value="2017">2017</option>
-          <option value="2018">2018</option>
-          <option value="2019">2019</option>
-          <option value="2020">2020</option>
-          <option value="2021">2021</option>
-          <option value="2022">2022</option>
+          <option value="2010-2011">2010</option>
+          <option value="2011-2012">2011</option>
+          <option value="2012-2013">2012</option>
+          <option value="2013-2014">2013</option>
+          <option value="2014-2015">2014</option>
+          <option value="2015-2016">2015</option>
+          <option value="2016-2017">2016</option>
+          <option value="2017-2018">2017</option>
+          <option value="2018-2019">2018</option>
+          <option value="2019-2020">2019</option>
+          <option value="2020-2021">2020</option>
+          <option value="2021-2022">2021</option>
+          <option value="2022-2023">2022</option>
         </select>
       </div>
 
@@ -84,13 +78,14 @@ const Standings = () => {
                 <span>
                   {`${index + 1}.`}
                   <img
-                    src={data.team.logos[0]?.href}
+                    src={data.strTeamBadge}
                     alt="#"
                     className="logo-small"
                   />
                 </span>{" "}
-                {data.team.shortDisplayName}
+                {data.strTeam}
               </h1>
+              <p>MP: {data.intPlayed} | W: {data.intWin} | D: {data.intDraw} | L: {data.intLoss} | GD: {data.intGoalDifference} | Pts: {data.intPoints} </p>
             </div>
           ))
         )}
